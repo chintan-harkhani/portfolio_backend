9e68aa853fc2d60192fc9da04484fbab513e0462
chintan-harkhani <harkhanichintan943@gmail.com>
2025-08-08 21:08:46 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import 'module-alias/register';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
