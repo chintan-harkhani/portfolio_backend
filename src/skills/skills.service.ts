b32d6d639fbb1fab29550e82831bf1d7bd5f8ca6
chintan-harkhani <harkhanichintan943@gmail.com>
2025-08-10 13:26:10 +0530

    uri update

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -4,14 +4,18 @@ import { UpdateSkillDto } from './dto/update-skill.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Skill, SkillDocument } from './skills.schema';
 import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SkillsService {
   constructor(
     @InjectModel(Skill.name) private skillModel: Model<SkillDocument>,
+      private configService: ConfigService,
   ) { }
   async create(skillName: string, iconFilename: string): Promise<Skill> {
-    const iconUrl = `http://localhost:3001/uploads/${iconFilename}`;
+    // const iconUrl = `http://localhost:3001/uploads/${iconFilename}`;
+    const baseUrl = this.configService.get<string>('BASE_URL'); // from .env
+    const iconUrl = `${baseUrl}/uploads/${iconFilename}`;
     const newSkill = new this.skillModel({ skillName, iconUrl });
     return newSkill.save();
   }
